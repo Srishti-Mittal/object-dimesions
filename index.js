5004606d41f1aa22b2dbaa40fb4ed92f2ec6243f
Srishti-Mittal <st150810@gmail.com>
2021-09-20 17:52:17 +0530

    heroku check

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/upload', upload.single('upload'), (req, res) => {
           imgArray.push("output/"+i+".jpeg")
         } 
         console.log(imgArray)
-        res.render('pages/image',{data: "output/0.jpeg",imgs:imgArray});
+        res.render('pages/image',{data: "output/0.jpeg",imgs:[ 'output/0.jpeg', 'output/1.jpeg', 'output/2.jpeg', 'output/3.jpeg' ]});
 
       })
     
